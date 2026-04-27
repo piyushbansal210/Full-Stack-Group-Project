31b3c2990f44842f3aacbfb9c435a97394a75419
Josh Berman <joshberman@ssw.com.au>
2026-04-27 10:02:45 +0200

    cleanup: import cleanup

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
 import App from "../App";
 import ErrorPage from "../pages/error-page";
 
