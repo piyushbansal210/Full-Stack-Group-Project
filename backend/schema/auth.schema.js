3c5fcfc8efff0332f545deb679b6ec486f3faea5
Josh Berman <joshberman@ssw.com.au>
2026-04-27 16:26:37 +0200

    fix: remove max constraints on zod schema

--- backend/schema/auth.schema.js
+++ backend/schema/auth.schema.js
@@ -1,18 +1,15 @@
 import z from "zod";
 
 const loginSchema = z.object({
-    username: z.string().min(3).max(20),
-    password: z.string().min(8).max(20),
+    username: z.string().min(3),
+    password: z.string().min(8),
 })
 
 
 const registerSchema = z.object({
-    username: z.string().min(3, 'Username must be at least 3 characters').max(20, 'Username must be less than 20 characters'),
+    username: z.string().min(3, 'Username must be at least 3 characters'),
     password: z.string().min(8, 'Password must be at least 8 characters'),
     confirmPassword: z.string().min(8)
-}).refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
 })
 
 export const authSchema = {
