82ee8bff0014586d6cac74d1e47775d71ab78fd4
Josh Berman <joshberman@ssw.com.au>
2026-04-27 12:41:05 +0200

    fix: jwt import

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { authSchema } from '../schema/auth.schema.js';
 import User from '../models/user.js';
+import jwt from 'jsonwebtoken';
 
 export const login = async (req, res) => {
     const { success, data, error } = authSchema.loginSchema.safeParse(req.body);
@@ -19,8 +20,8 @@ export const login = async (req, res) => {
             return res.status(400).json({ message: 'Invalid password' });
         }
 
-        const token = signToken(user);
-        
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
         return res.status(200).json({ message: 'Login successful', user: user.toJSON(), token });
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error', error: error.message });
@@ -40,7 +41,7 @@ export const register = async (req, res) => {
         }  
 
         const user = await User.create(data);
-        const token = signToken(user);
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         return res.status(201).json({ message: 'User created successfully', user: user.toJSON(), token });
     } catch (error) {
