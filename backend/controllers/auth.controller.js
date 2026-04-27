c467f040187adf8caf7bff90f678cd5cec444dcb
Josh Berman <joshberman@ssw.com.au>
2026-04-27 12:34:58 +0200

    fix: signToken for login and register routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -19,9 +19,9 @@ export const login = async (req, res) => {
             return res.status(400).json({ message: 'Invalid password' });
         }
 
-
-        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        return res.status(200).json({ message: 'Login successful', token });
+        const token = signToken(user);
+        
+        return res.status(200).json({ message: 'Login successful', user: user.toJSON(), token });
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error', error: error.message });
     }
@@ -33,15 +33,16 @@ export const register = async (req, res) => {
     if (error) {
         return res.status(400).json({ message: 'Invalid data', error: error.message });
     }
-
     try {
         const userExists = await User.findOne({ username: data.username });
         if (userExists) {
             return res.status(400).json({ message: 'User already exists' });
-        }        
+        }  
+
         const user = await User.create(data);
-        console.log(user);
-        return res.status(201).json({ message: 'User created successfully', user });
+        const token = signToken(user);
+
+        return res.status(201).json({ message: 'User created successfully', user: user.toJSON(), token });
     } catch (error) {
         return res.status(500).json({ message: 'Internal server error', error: error.message });
     }
