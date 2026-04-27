2b146c1693fb9cdaf064e8b3e02a062ea2a04a31
Josh Berman <joshberman@ssw.com.au>
2026-04-27 12:30:24 +0200

    add: login and register routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,48 @@
+import { authSchema } from '../schema/auth.schema.js';
+import User from '../models/user.js';
+
 export const login = async (req, res) => {
-    console.log('Login');
-    res.status(200).json({ message: 'Login successful' });
+    const { success, data, error } = authSchema.loginSchema.safeParse(req.body);
+
+    if (error) { 
+        return res.status(400).json({ message: 'Invalid data', error: error.message });
+    }
+
+    try {
+        const user = await User.findOne({ username: data.username });
+        if (!user) {
+            return res.status(400).json({ message: 'User not found' });
+        }
+
+        const isPasswordCorrect = await user.comparePassword(data.password);
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: 'Invalid password' });
+        }
+
+
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        return res.status(200).json({ message: 'Login successful', token });
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
 }
 
 export const register = async (req, res) => {
-    console.log('Register');
-    res.status(200).json({ message: 'Register successful' });
+    const { success, data, error } = authSchema.registerSchema.safeParse(req.body);
+
+    if (error) {
+        return res.status(400).json({ message: 'Invalid data', error: error.message });
+    }
+
+    try {
+        const userExists = await User.findOne({ username: data.username });
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }        
+        const user = await User.create(data);
+        console.log(user);
+        return res.status(201).json({ message: 'User created successfully', user });
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
 }
